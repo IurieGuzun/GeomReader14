9f6f3d2ab60a3947daba44a2b5623dda0611868d
Iurie Guzun <iurie_guzun@yahoo.com>
2021-02-09 17:03:43 -0500

    Initial Commit

---
+++ GeomReader14/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeomReader14
+//
+//  Created by Iurie Guzun on 2021-02-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeomReader14/GeomReader14App.swift
@@ -0,0 +1,17 @@
+//
+//  GeomReader14App.swift
+//  GeomReader14
+//
+//  Created by Iurie Guzun on 2021-02-09.
+//
+
+import SwiftUI
+
+@main
+struct GeomReader14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
